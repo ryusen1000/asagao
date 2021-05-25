4c6f920d1ae97c3c26c8165d05aaa3702c96bded
USER <81149239+ryusen1000@users.noreply.github.com>
2021-05-26 00:12:37 +0900

    indexのリンク編集とCSSエラー修正

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,24 @@
+class MembersController < ApplicationController
+  def index
+    @members = Member.order("number")
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
