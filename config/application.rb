b064b0287485ade59c550d2e12f2946aa6d5c05c
USER <81149239+ryusen1000@users.noreply.github.com>
2021-05-27 16:27:20 +0900

    commit8バリデーションと日本語化

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module Asagao
     config.generators.system_tests = nil
 
     config.time_zone = "Tokyo"
-
+    config.i18n.default_locale = :ja
     config.action_controller.permit_all_parameters = true
   end
 end
