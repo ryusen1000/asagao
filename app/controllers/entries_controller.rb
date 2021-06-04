28d03edf22ee157984f6bd18fd6164a38113a53e
USER <81149239+ryusen1000@users.noreply.github.com>
2021-06-04 11:37:25 +0900

    commit29自分が投票した記事一覧

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -57,6 +57,17 @@ class EntriesController < ApplicationController
     redirect_to @entry, notice: "投票しました。"
   end
 
+  def unlike
+    current_member.voted_entries.destroy(Entry.find(params[:id]))
+    redirect_to :voted_entries, notice: "削除しました。"
+  end
+
+  def voted
+    @entries = current_member.voted_entries.published
+      .order("votes.created_at DESC")
+      .page(params[:page]).per(15)
+  end
+
   private def entry_params
     params.require(:entry).permit(
       :member_id,
